219a4fa9ed801c185c77aa1a5cfaaf3c11b336a6
Quildreen Motta <quildreen@gmail.com>
2011-09-09 13:56:39 -0300

    Rewriting the library for being prototypical centred.

--- src/boo.js
+++ src/boo.js
@@ -1,329 +1,147 @@
 /******************************************************************************
  *                                   ~boo~                                    *
  *                                 ‾‾‾‾‾‾‾‾‾                                  *
- * Provides prototypal utilities for inheritance + basic traits system.       *
+ * Provides prototypal utilities for OO.                                      *
  *     _________________________________________________________________      *
  *        Copyright (c) 2011 Quildreen Motta // Licenced under MIT/X11        *
  ******************************************************************************/
 
-//// Module boo ////////////////////////////////////////////////////////////////
-//
-// The goal is to provide a handier prototypal inheritance structure for
-// JavaScript programs, by adding new functions to deal with the usual
-// pattern in an abstract way, as well as introducing a new system to
-// deal with *multiple-inheritance* — traits.
-//
-// Note that the Traits system implemented by this library is not the
-// same as the one in Self; This system is, in fact, closer to `mixins`
-// than actual Traits.
+/// Module boo /////////////////////////////////////////////////////////////////
+void function (root) {
 
+    var __old, boo
 
-//// -Objects and inheritance //////////////////////////////////////////////////
-//
-// There are a handful of problems with the way JavaScript handles
-// inheritance out of the box. And I'm not talking about being
-// Prototypal — since that's one of the best things about it.
-//
-// For one, you can't inherit directly from other objects. ES5
-// introduced Object.create, but it's not available everywhere.
-//
-// There's no `extend` function built-in, which means that once you've
-// set the prototype for a constructor, you have to manually set all of
-// the functions too. This also means you don't get to extend the
-// prototype with other objects very nicely (out of the box, of
-// course).
-//
-// Calling `super` methods requires hard-coded parents, which doesn't
-// work when you have things like traits extended directly on the
-// prototype. Capatibilities of parents aren't implemented either —
-// testing which one implements which attributes.
-//
-// To solve this, the module provides three new functions: `inherit`,
-// `upper` and `extend`, which are discussed in more detail below.
-
-void function (root) { var boo, old
-
-    // Some alias for rather JavaLongCommandNamesAndIReallyMeanLong.
-    , proto  = Object.getPrototypeOf
-    , keys   = Object.keys
-    , create = Object.create
-    , slice  = Array.prototype.slice
-    , has    = Object.prototype.hasOwnProperty
-
-
-
-    function objp(obj) {
-        return Object(obj) === obj }
-
-    function fnp(obj) {
-        return typeof obj == 'function' }
-
-
-
-    ///// Function inherit /////////////////////////////////////////////////////
+    //// Function nplugin //////////////////////////////////////////////////////
     //
-    //   (ctor, base[, props]) ⇒ Function
+    //   (object:Obj, mixin:Obj) → Obj
     //
-    // Takes a constructor and a base object, and sets the prototype in
-    // the constructor without breaking `instanceof`.
+    // Copies the own enumerable properties from the mixin over to the
+    // ~object~.
     //
-    // :param: {Function} ctor
-    // :param: {Object}   base
-    // :param: {Object}   props
-    //
-    // If an additional object is passed (as `props`), the properties
-    // defined in that object will be copied into the prototype of the
-    // constructor as well.
-    //
-    // Note that these *additional properties* won't have a special
-    // inheritance relationship with the constructor's prototype. In
-    // fact, they **will** be treated as just being the constructor's
-    // own prototype properties.
+    // Only /own/ and /enumerable/ properties present on the ~mixin~
+    // will be copied, this ensures the function works the same even in
+    // environments that don't support ECMAScript 5 object additions for
+    // inspecting properties.
     //
     // :warning: side-effects
-    //    The given constructor and properties are modified in-place.
+    //   The function will modify ~object~ in-place. For a pure version,
+    //   see [[fn:plugin]].
     //
-    function inherit(ctor, base, props) {
-        if (!props) props = {}
 
-        props.constructor = ctor
-        props.$boo_super  = base
-        ctor.prototype    = create(base)
-        return extend(ctor.prototype, props)
-    }
+    //// Function plugin ///////////////////////////////////////////////////////
+    //
+    //   (object:Obj, mixin:Obj) → Obj
+    //
+    // Copies the own enumerable properties from the mixin over to the
+    // ~object~.
+    //
+    // This is done without modifying the ~object~ (if you want in-place
+    // modification for performance or something, use
+    // [[!nplugin]]). Only the /own enumerable/ properties, alongside
+    // with the prototype chain of the ~object~ will be carried over to
+    // the clone.
+    //
+    // :see-also:
+    //   - [[fn:nplugin]] — the destructive (more reliable) version.
+    //   - [[fn:clone]]   — the object cloning routine used for purity.
+    //
 
+    //// Function merge ////////////////////////////////////////////////////////
+    //
+    //   (mixins:Obj...) → Obj
+    //
+    // Merges all given mixins into a single, fresh object.
+    //
+    // Only the own enumerable properties of each mixin are carried over
+    // to the fresh object.
+    //
+    // This copying is done in order, from left to right. If a previous
+    // mixin defines a property ~foo~, and a latter mixin also defines a
+    // property ~foo~, the latter mixin's property value will overwrite
+    // the previous value.
+    //
 
-    ///// Function extend //////////////////////////////////////////////////////
+    //// Function clone ////////////////////////////////////////////////////////
     //
-    //   (target, sources...) ⇒ Object
+    //   (object:Obj) → Obj
     //
-    // Copies the given source's **own** enumerable properties in `target`.
+    // Returns a deep clone of the ~object~, but carrying over only the
+    // /own enumerable/ properties and the prototype chain.
     //
-    // :param: {Object} target
-    // :param: {Object} sources...
+    // This is done in order to make it work on ancient environments
+    // which don't implement the ECMAScript 5 methods for inspecting an
+    // object's properties.
     //
-    // :note:
-    //    This is only a **shallow** copy, anything other than
-    //    primitives will be copied just as a reference to the original
-    //    object.
-    //
-    // :warning: side-effects
-    //    The given `obj` is modified in-place.
-    //
-    function extend(target) { var sources
-        sources = slice.call(arguments, 1)
 
-        sources.forEach(function(source) {
-            keys(source).forEach(function(key) {
-                target[key] = source[key] })})
+    //// Function inherit //////////////////////////////////////////////////////
+    //
+    //   (proto:Obj, mixins:Obj...) → Obj
+    //
+    // Creates a new object using ~proto~ as the ~[⁣[Prototype]⁣]~, and
+    // extending it with the given mixins.
+    //
+    // Mixin extension is done from left to right, as described in
+    // [[fn:merge]].
+    //
 
-        return target
-    }
-
-
-    ///// Function clone ///////////////////////////////////////////////////////
+    //// Function receiver /////////////////////////////////////////////////////
     //
-    //   (base[, extensions...]) ⇒ Object
+    //   (base:Obj, message:String[, allow_mixins:Bool = true]) → Obj
     //
-    // Creates a new object by inheriting `base' and populating it with
-    // the given extensions.
+    // Finds the closest ancestor of ~base~ which can handle the given
+    // ~message~.
     //
-    // :param: {Object} base
-    // :param: {Object} extensions...
-    //
-    function clone(obj) { var result
-        result = create(obj)
-        return extend.apply(result, slice.call(arguments, 1))
-    }
-
-
-
-    ///// Function find_able ///////////////////////////////////////////////////
-    //
-    //   (obj, attribute[, allow_traits = true]) ⇒ Object
-    //
-    // Searches which of the parents of the given objects implement the
-    // attribute, and returns it.
-    //
-    // :param: {Object}  obj
-    // :param: {String}  attribute
-    // :param: {Boolean} allow_traits
-    //
-    // Sometimes you want to know which object is capable of doing
-    // something, and most of the times a simple `in` check will be
-    // enough. For those times where you want to check which of the
-    // **ancestors** of an object implement a certain functionality (for
-    // calling a `super` method, for example), you have this method.
-    //
-    // By default, this function will search any of the objects that
-    // provides the requested functionality for the base object —
-    // traits included.
+    // Sometimes you want to know which ancestor exactly implements a
+    // given message (~property~), and in this case a simple ~in~ check
+    // will not do. This method will search up over the ancestor chain
+    // of a base object, optionally including the mixins that were set
+    // on it, and find the first ancestor that can receive that
+    // message.
     //
     // The prototype is searched first, then, if the functionality is
-    // not found, we move on to searching the trait list, in the reverse
-    // order they were plugged in.
+    // not found, we move on to searching the mixin list (in the reverse
+    // order they were plugged in).
     //
-    // If the method is not found in the immediate ancestors, the
-    // function will keep searching up the prototype chain to try to
-    // find the requested functionality. And if it really can't find the
-    // attribute, `null` is returned.
+    // If no immediate ancestor implements the message, the function
+    // will keep searching up the prototype chain to try to find the
+    // requested functionality. If it definitely can't find the any
+    // receiver, ~null~ is returned.
     //
-    // > Note that the trait's prototypes and the object itself are
-    // > **not** included in this search chain.
+    // :note:
+    //   the mixin's prototypes and the base object itself are
+    //   *not* included in this search chain. Mixins are expected to be
+    //   parent-less anyways.
     //
-    function find_able(obj, attribute, allow_traits) { var bases, cur
-        function get_base(obj) { return obj.$boo_super || obj           }
-        function has_attr()    { return cur && has.call(cur, attribute) }
 
-        if (allow_traits == null) allow_traits = true
-        while (obj) {
-            bases = [get_base(obj)]
-            if (allow_traits) bases.concat(obj.$boo_traits || [])
-
-            while (bases.length) {
-                cur = bases.shift()
-                if (has_attr()) return get_base(obj) }
-
-            obj = proto(obj) }
-    }
-
-
-
-    ///// Function upper ///////////////////////////////////////////////////////
+    //// Function receivers ////////////////////////////////////////////////////
     //
-    //   (obj, base, method, args...) ⇒ *mixed*
+    //   (base:Obj, message:String[, allow_mixins:Bool = true]) → Obj
     //
-    // Calls a parent method in the context of the given object.
+    // Finds all the ancestors of ~base~ who can handle the given
+    // ~message~.
     //
-    // :param: {Object} obj
-    // :param: {Object} base
-    // :param: {String} method
+    // The algorithm works similarly to [[fn:receiver]], except that it
+    // does not stop on the first receiver.
     //
-    // This allows for fully prototypal inheritance, without loosing
-    // overwritten methods. Because, you see, we assume that methods
-    // that were overwritten are somehow **important** and you will want
-    // to refer to them on your functions.
+    // :see-also:
+    //   - [[fn:receiver]] — finds only the first receiver for the
+    //                       message.
     //
-    // The given method will **always** be searched on all of the
-    // ancestors, traits included, and it'll be called in the context of
-    // the given object.
-    //
-    // To avoid errors with *endless recursion*, the super call state
-    // (the object that was looked upon last time) is saved on each
-    // call, and removed when the function returns.
-    //
-    // This is done by simple writing to the `$boo_ctx` property, and
-    // expects your function to be **synchronous**. If you're calling
-    // any asynchronous super method, you'll need to pass the previous
-    // stored context explicitly:
-    //
-    //     boo.upper(this, this.$boo_ctx, "show")
-    //
-    // :warning: potentially unsafe
-    //    This assumes you won't be writing to `$boo_ctx` in your
-    //    code. As a rule of thumb, you shouldn't ever have an actual
-    //    property with a dollar sign anyways.
-    //
-    function upper(obj) { var args, base, method
-        args = slice.call(arguments, 1)
-        base = args.shift()
-
-        if (!objp(base)) {
-            method = base
-            base   = null }
-        else
-            method = args.shift()
-
-        if (!base)
-            base = obj.$boo_ctx || obj
-
-        return find_ancestor(obj, base, method, args)
-    }
-
-    // try to find the first ancestor to implement the method, then
-    // return the result of calling this method.
-    function find_ancestor(obj, base, method, args) { var parent, rv
-        parent       = find_able(base, method)
-        obj.$boo_ctx = parent
-
-        rv = parent[method].apply(obj, args)
-        delete obj.$boo_ctx
-        return rv
-    }
-
 
 
-    ///// Function plugin //////////////////////////////////////////////////////
-    //
-    //   (obj, traits...) ⇒ Function | Object
-    //
-    // Takes a constructor or an object, and adds traits to the
-    // prototype, returning the given object/constructor.
-    //
-    // :param: {Function | Object} obj
-    // :param: {Object}            traits...
-    //
-    // Traits are a mechanism to handle the same issue as
-    // multiple-inheritance, but in a Prototypal OO manner. The idea is
-    // that traits provide just a collection of, perhaps, incomplete
-    // functionality that's intended to be plugged in an object.
-    //
-    // This is more powerful than the classical multiple-inheritance
-    // system, because you can plugin traits in the constructor, long
-    // after several instances of an object have been created, and all
-    // of those instances will magically have that trait available too:
-    //
-    //     function Mage() {}
-    //     Mage.cast = function(spell){ return "You've cast " + spell }
-    //
-    //     var RedMage = { fira: function(){ this.cast("Fira") }}
-    //     var Vivi    = new Mage
-    //     Vivi.cast("Blizzard") // You've cast Blizzard
-    //
-    //     // Makes all Mages learn fire magic
-    //     boo.plugin(Mage, RedMage)
-    //     Vivi.fira()          // You've cast Fira
-    //
-    // Not only this, but the traits plugged to a constructor are
-    // tracked, and you can call their methods as `super` methods. And
-    // as with the prototypal inheritance, you don't need to create a
-    // separate meta-object for your functionality, ANY object can be a
-    // trait :3
-    //
-    // :warning: side-effects
-    //    The given `obj` is modified in-place.
-    //
-    function plugin(obj) { var args, base, ctor
-        function get_traits() { return (ctor.$boo_traits || []).concat(args) }
+    ///// Exports //////////////////////////////////////////////////////////////
+    if (typeof exports == 'undefined') {
+        __old = root.boo
+        boo   = root.boo = {}
 
-        args = slice.call(arguments, 1)
-        base = fnp(obj)? obj.prototype
-                       : proto(obj)
-        ctor = base.constructor
-
-        ctor.$boo_traits = get_traits()
-        extend.apply(obj, [base].concat(args))
-        return obj
-    }
-
-
-
-    ///// Exports ////////////////////////////////////////////////////////////
-    if (typeof exports == "undefined") {
-        old = root.boo
-        boo = root.boo = {}
-
+        ///// Method boo.make_local ////////////////////////////////////////////
         boo.make_local = function() {
-            root.boo = old
+            root.boo = __old
             return boo }}
     else
         boo = exports
 
-    ////// -Properties under boo ///////////////////////////////////////////////
-    boo.inherit   = inherit
-    boo.extend    = extend
-    boo.clone     = clone
-    boo.find_able = find_able
-    boo.upper     = upper
-    boo.plugin    = plugin
-}(this);
+    ///// -Properties under boo ////////////////////////////////////////////////
+
+
+}(this)
