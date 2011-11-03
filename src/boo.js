0d2d2c8694578b3b11ed2cebd550356d7f7e1ded
Quildreen Motta <quildreen@gmail.com>
2011-11-02 22:38:10 -0200

    Fixes syntax error in `objp' predicate function.

--- src/boo.js
+++ src/boo.js
@@ -16,86 +16,92 @@ void function (root) {
 
 
 
-    //// -Error handling ///////////////////////////////////////////////////////
+    //// -Error handling
 
     ///// Function key_collision_error /////////////////////////////////////////
-    //
-    // : (key:Str) →
     //
     // Throws an error signaling that a key collision happened when
     // doing trait composition.
     //
     // The property names will have to be manually resolved by the
-    // programmer.
+    // programmer, by using the provided collision handling functions.
     //
+    // :thows: Error
+    // :: key:Str → Undefined
     function key_collision_error(key) {
         throw new Error('"' + key + '" already exists.')
     }
 
 
 
-    //// -Interface testing ////////////////////////////////////////////////////
+    //// -Interface testing
+
+    ///// Function objp ////////////////////////////////////////////////////////
+    //
+    // Checks if something is an object, as opposed to a primitive.
+    //
+    // Obj :: subject:Any → Bool
+    function objp(subject) {
+        return Object(subject) === subject
+    }
 
     ///// Function clonablep ///////////////////////////////////////////////////
-    //
-    // : (object:Obj) → Bool
     //
     // Checks if an object implements a clone factory.
     //
     // A clonable object is any object that implements the special
-    // ~__clone__~ method. The clone method is called without arguments
+    // =__clone__= method. The clone method is called without arguments
     // and is expected to return an object.
     //
-    function clonablep(object) {
-        return Object(object) === object
-            && typeof object.__clone__ == 'function'
+    // Clonable :: subject:Obj → Bool
+    function clonablep(subject) {
+        return objp(subject)
+            && typeof subject.__clone__ == 'function'
     }
-
 
     ///// Function callablep ///////////////////////////////////////////////////
     //
-    // : (object:Obj) → Bool
-    //
     // Checks if the object can be called directly.
     //
-    function callablep(object) {
-        return typeof object == 'function'
+    // Callable :: object:Obj → Bool
+    function callablep(subject) {
+        return typeof subject == 'function'
     }
-
 
     ///// Function testablep ///////////////////////////////////////////////////
     //
-    // : (object:Obj) → Bool
+    // Checks if the object implements at =test= function.
     //
-    // Checks if the object implements at ~test~ function.
+    // Testable objects are expected to implement a =test= predicate
+    // that takes a single argument, an Object, evaluates it and returns
+    // whether it passed the test or not.
     //
-    function testablep(object) {
-        return object
-            && typeof object.test == 'function'
+    // Testable :: subject:Obj → Bool
+    function testablep(subject) {
+        return objp(subject)
+            && typeof subject.test == 'function'
     }
 
 
 
-    //// -Object Orientation Utilities /////////////////////////////////////////
+    //// -Object Orientation Utilities
 
     ///// Function extend //////////////////////////////////////////////////////
     //
-    // : (object:Obj, mixins:Array) → Obj
-    //
     // Copies all own enumerable properties from the mixins over to the
-    // ~object~.
+    // =object=.
     //
-    // Only /own/ and /enumerable/ properties present on the ~mixins~
+    // Only /own/ and /enumerable/ properties present on the =mixins=
     // will be carried over, this ensures that the function works the
     // same, even on environments that don't support ECMAScript 5
     // additions for inspecting properties.
     //
-    // The copying is done in order, from left to right, such that if a
-    // mixin defines a property ~foo~, and a latter mixin also defines a
-    // property foo, the latter mixin's property value will overwrite
-    // the previous value.
+    // The copying is done in order, from left to right, with rightmost
+    // priority such that if a mixin defines a property =foo=, and a
+    // latter mixin also defines a property =foo=, the latter mixin's
+    // property value will overwrite the previous value.
     //
-    // If the mixin provides a ~clone~ method, that method is expected
+    // If the mixin is =Clonable=, its =__clone__= method is expected
     // to be called with no arguments and return a new object. The
     // returned object will be copied over instead of the original
     // one. This allows the mixin to provide a way of initialising
@@ -105,6 +111,7 @@ void function (root) {
     // :warning: side-effects
     //    The function will modify the ~object~ in-place.
     //
+    // :: object:Obj, [mixins:Obj] → Obj
     function extend(object, mixins) { var i, j, key, len, mixin, props
         for (i = 0, len = mixins.length; i < len; ++i) {
             mixin = clonablep(mixins[i])?  mixins[i].__clone__()
@@ -117,10 +124,7 @@ void function (root) {
         return object
     }
 
-
     ///// Function merge ///////////////////////////////////////////////////////
-    //
-    // : (mixins:Obj...) → Obj
     //
     // Merges all the given mixins into a single, fresh object.
     //
@@ -132,61 +136,53 @@ void function (root) {
     // :see-also:
     //    - [[fn:extend]] — The semantics on carrying properties over.
     //
+    // :: mixins:Obj... → Obj
     function merge() {
         return extend({}, arguments)
     }
 
-
     ///// Function clone ///////////////////////////////////////////////////////
     //
-    // : (proto:Obj, mixins:Obj...) → Obj
-    //
-    // Creates a new object, inheriting from the given ~proto~.
+    // Creates a new object, inheriting from the given =prototype=.
     //
     // The resulting fresh object is also extended with the provided
     // mixins, if any, from left to right — where properties at right
     // have higher precedence.
     //
-    // If the object implements the special method ~__init__~, that will
-    // be called to initialise the instance.
-    //
     // :see-also:
     //    - [[fn:extend]] — The semantics on copying mixins.
     //
+    // :: proto:Obj, mixins:Obj... → Obj
     function clone(proto) {
         return extend(make_obj(proto), slice.call(arguments, 1))
     }
 
-
     //// Function proto ////////////////////////////////////////////////////////
     //
-    // : (object:Obj) → Obj
+    // Returns the =[⁣[Prototype]⁣]= of the given object.
     //
-    // Returns the prototype of the given object.
-    //
+    // :: object:Obj → Obj
     // :alias: Object.getPrototypeOf
-
 
     ///// Function compose /////////////////////////////////////////////////////
     //
-    // : (proto:Obj, traits:Obj...) → Obj
+    // Creates a new object, inheriting from the given =prototype= and
+    // implementing the provided =traits=.
     //
-    // Creates a new object, inheriting from the given ~proto~ and
-    // implementing the provided ~traits~.
-    //
-    // Differently from the simple ~extend~ approach, though, the
-    // ~compose~ function doesn't use property precedence, instead,
+    // Differently from the simple =extend= approach, though, the
+    // =compose= function doesn't use property precedence, instead,
     // trying to add duplicate properties to an object will throw an
     // error.
     //
     // This is so that the developer can catch these errors and
     // fix them, by mapping property names or by selectively importing
-    // names from objects. A ~resolve~ function is provided to take care
+    // names from objects. A =resolve= function is provided to take care
     // of such things.
     //
     // :see-also:
     //    - [[fn:resolve]] — Resolve name conflicts.
     //
+    // :: proto:Obj, traits:Obj... → Obj
     function compose(proto) { var i, j, key, trait, traits, object, props
         traits = slice.call(arguments, 1)
         object = clone(proto)
@@ -204,14 +200,16 @@ void function (root) {
 
 
 
-    //// -Conflict resolution //////////////////////////////////////////////////
+    //// -Conflict resolution
 
     ///// Function resolve /////////////////////////////////////////////////////
     //
-    // : (object:Object, mappings:Object) → Object
+    // Returns a new object, with its own properties ramapped, using the
+    // provided rules.
     //
-    // Returns a new object, with its own properties ramapped.
     //
+    //
+    // :: object:Obj, mappings:Obj → Obj
     function resolve(object, mappings) { var result, props, map, new_key
         result = clone(proto(object))
         props  = keys(object)
@@ -228,7 +226,6 @@ void function (root) {
 
         return result
     }
-
 
     ///// Function take_keyp ///////////////////////////////////////////////////
     //
@@ -255,7 +252,6 @@ void function (root) {
         return keys.filter(take_keyp(filter))
     }
 
-
     ///// Function exclude /////////////////////////////////////////////////////
     //
     // : (keys:Array, filter:Array|RegExp|Fn) → Array
@@ -267,7 +263,6 @@ void function (root) {
         filter = take_keyp(filter)
         return keys.filter(function(key){ return !filter(key) })
     }
-
 
     ///// Function rename //////////////////////////////////////////////////////
     //
